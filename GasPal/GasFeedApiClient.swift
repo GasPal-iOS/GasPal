5e392f69115d0ffcb9d9680fc493ce40ced77ce2
dkumawat <diwakar_kumawat@intuit.com>
2017-04-28 14:06:08 -0700

    clients

---
+++ GasPal/GasFeedApiClient.swift
@@ -0,0 +1,13 @@
+//
+//  GasFeedApiClient.swift
+//  GasPal
+//
+//  Created by Kumawat, Diwakar on 4/28/17.
+//  Copyright © 2017 Tyler Hackley Lewis. All rights reserved.
+//
+
+import UIKit
+
+class GasFeedApiClient: NSObject {
+
+}
