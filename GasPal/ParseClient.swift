abed06e090226611e613970e8bb5fd92e1e639c9
Luis Rocha <lerocha@gmail.com>
2017-04-29 15:39:05 -0700

    issue #1: initial implementation of ParseClient

--- GasPal/ParseClient.swift
+++ GasPal/ParseClient.swift
@@ -7,7 +7,51 @@
 //
 
 import UIKit
+import Parse
 
 class ParseClient: NSObject {
+    
+    static let sharedInstance = ParseClient()
+    
+    override init() {
+        super.init()
+        // Initialize Parse
+        // Set applicationId and server based on the values in the Heroku settings.
+        // clientKey is not used on Parse open source unless explicitly configured
+        Parse.initialize(
+            with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) -> Void in
+                configuration.applicationId = "GasPal-Parse"
+                configuration.clientKey = nil
+                configuration.server = "http://gaspal-parse.herokuapp.com/parse"
+            })
+        )
+    }
+    
+    func signUp(email: String!, password: String!, success: @escaping (PFUser) -> (), failure: @escaping (Error) -> ()) {
+        let user = PFUser()
+        user.email = email
+        user.username = email
+        user.password = password
+        user.signUpInBackground { (succeeded: Bool, error: Error?) in
+            if let error = error {
+                print("signUp; status=failed; error=\(error.localizedDescription)")
+                failure(error)
+            } else {
+                print("signUp; status=success; user=\(user.username!)")
+                success(user)
+            }
+        }
+    }
 
+    func login(email: String!, password: String!, success: @escaping (PFUser) -> (), failure: @escaping (Error) -> ()) {
+        PFUser.logInWithUsername(inBackground: email, password: password) { (user: PFUser?, error: Error?) in
+            if let error = error {
+                print("login; status=failed; error=\(error.localizedDescription)")
+                failure(error)
+            } else {
+                print("login; status=success; user=\(user!.username!)")
+                success(user!)
+            }
+        }
+    }
 }
