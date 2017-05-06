fe8518542fb81453801bcf30ab78839c8e05d2a8
Luis Rocha <lerocha@gmail.com>
2017-05-06 12:14:14 -0700

    Adding controller to add/edit Tracking logs

---
+++ GasPal/TrackingDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TrackingDetailViewController.swift
+//  GasPal
+//
+//  Created by Luis Rocha on 5/6/17.
+//  Copyright © 2017 Tyler Hackley Lewis. All rights reserved.
+//
+
+import UIKit
+
+class TrackingDetailViewController: UIViewController {
+
+    var tracking: TrackingModel?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
